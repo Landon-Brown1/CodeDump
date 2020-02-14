8da44a7bac63faa118294fc5c728b3e4588ca9a4
Landon Brown <lmbrown0908@my.msutexas.edu>
2020-02-13 19:40:26 -0600

    created filetypes for all environments

---
+++ Languages/C#/Basics.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Basics{
+    class Program{
+        static void main(string[] args){
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
